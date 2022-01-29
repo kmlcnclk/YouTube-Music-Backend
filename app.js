d185a5c0b55d444c82188bbd6c7bcbb1adb05e4a
Mr.Steelcan <mr.steelcan@gmail.com>
2022-01-29 11:19:29 +0200

    Update app.js

--- app.js
+++ app.js
@@ -35,6 +35,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(mainRouter);
 app.use('/public', express.static(path.join(__dirname, 'public'), options));
+app.get('/', (req, res) => {
+  res.json({ zxc: 'Not Found' });
+});
+
 app.use('*', (req, res) => {
   res.send('Not Found');
 });
