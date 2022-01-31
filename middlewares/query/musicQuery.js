dc67d9213a10de4dd8657d6d20c0bf4fe269a5e1
Mr.Steelcan <mr.steelcan@gmail.com>
2022-01-31 10:13:17 +0200

    deploy

--- middlewares/query/musicQuery.js
+++ middlewares/query/musicQuery.js
@@ -1,44 +1,42 @@
-const asyncHandler = require('express-async-handler');
 const {
   populateHelper,
   paginationHelper,
 } = require('./queryMiddlewareHelpers');
 const MusicModel = require('../../databases/models/MusicModel');
+const expressAsyncHandler = require('express-async-handler');
 
 // Music Query Middleware
-const musicQueryMiddleware = function (options) {
-  return asyncHandler(async (req, res, next) => {
-    let query = MusicModel.find()
-      .populate({
-        path: 'artists',
-        select: 'name',
-      })
-      .populate({
-        path: 'album_or_single',
-        select: 'name',
-      });
+const musicQueryMiddleware = expressAsyncHandler(async (req, res, next) => {
+  let query = MusicModel.find()
+    .populate({
+      path: 'artists',
+      select: 'name',
+    })
+    .populate({
+      path: 'album_or_single',
+      select: 'name',
+    });
 
-    if (options && options.population) {
-      query = populateHelper(query, options.population);
-    }
+  if (options && options.population) {
+    query = populateHelper(query, options.population);
+  }
 
-    const total = await MusicModel.countDocuments();
-    const paginationResult = await paginationHelper(total, 20, query, req);
+  const total = await MusicModel.countDocuments();
+  const paginationResult = await paginationHelper(total, 20, query, req);
 
-    query = paginationResult.query;
-    const pagination = paginationResult.pagination;
+  query = paginationResult.query;
+  const pagination = paginationResult.pagination;
 
-    const queryResults = {
-      musics: await query,
-      pagination: await pagination,
-    };
+  const queryResults = {
+    musics: await query,
+    pagination: await pagination,
+  };
 
-    res.queryResults = {
-      success: true,
-      data: queryResults,
-    };
-    next();
-  });
-};
+  res.queryResults = {
+    success: true,
+    data: queryResults,
+  };
+  next();
+});
 
 module.exports = musicQueryMiddleware;
